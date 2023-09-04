198d0272e3156dd08bf88144d2250e4cef9ce211
Maxim Aglerimov <maxaglerimov@gmail.com>
2023-09-04 16:31:53 +0400

    Configured "curly" rule in ESLint

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,8 +33,7 @@ export class HeroDetailComponent implements OnInit {
   }
 
   save(): void {
-    if (this.hero) {
+    if (this.hero)
       this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
-    }
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,9 +23,8 @@ export class HeroesComponent implements OnInit {
 
   add(name: string): void {
     name = name.trim();
-    if (!name) {
-      return;
-    }
+    if (!name) return;
+
     this.heroService.addHero({ name } as Hero).subscribe(hero => {
       this.heroes.push(hero);
     });
