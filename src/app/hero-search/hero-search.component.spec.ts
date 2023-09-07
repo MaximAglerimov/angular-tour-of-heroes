193e393c98e69f2ca3f7787a827be2609afb19ef
Maxim Aglerimov <maxaglerimov@gmail.com>
2023-09-07 11:13:00 +0400

    Used HeroServiceMock instead of Spy

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -2,17 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroSearchComponent } from './hero-search.component';
 import { HeroService } from '../hero.service';
+import { HeroServiceMock } from 'src/tests/mocks';
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
   let fixture: ComponentFixture<HeroSearchComponent>;
 
   beforeEach(() => {
-    const heroServiceSpy = jasmine.createSpy('HeroService');
-
     TestBed.configureTestingModule({
       declarations: [HeroSearchComponent],
-      providers: [{ provide: HeroService, useValue: heroServiceSpy }],
+      providers: [{ provide: HeroService, useValue: HeroServiceMock }],
     });
 
     fixture = TestBed.createComponent(HeroSearchComponent);
