c7b7f74bac26e298f4a7d80ff3830ac83bc7403e
Maxim Aglerimov <maxaglerimov@gmail.com>
2023-09-07 17:52:50 +0400

    Added app.cy.ts to check the app's title

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,9 @@
+describe('App', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('has the correct title', () => {
+    cy.title().should('equal', 'AngularTourOfHeroes_wrong');
+  });
+});
